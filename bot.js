17e97ccb9d891db1129844ea1be4c8eec8fb75d2
mohamed-hsan <43474223+mohamed-hsan@users.noreply.github.com>
2018-09-21 18:05:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,27 +27,5 @@ client.user.setGame(`hack`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-client.on('message',async msg => {//Alpha Codes
-//Alpha Codes//Alpha Codes//Alpha Codes//Alpha Codes//Alpha Codes
-  var prefix = "=";//Alpha Codes
-//Alpha Codes
-  if(msg.content.startsWith(prefix + "user")) {//Alpha Codes
-  if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **go play minecraft**');
-  if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');
-  msg.guild.createChannel(`يتم تحضير الروم :[]` , 'voice').then(time => {
-    time.overwritePermissions(msg.guild.id, {
-      CONNECT: false,
-      SPEAK: false
-    });
-  setInterval(() => {
-      var currentTime = new Date(),
-Year = currentTime.getFullYear(),
-Month = currentTime.getMonth() + 1,
-Dat = currentTime.getDate()//Alpha Codes
-      time.setName(`Members : ◤ → ${client.users.size} ← ◢`);
- },1000);
-  });//Alpha Codes
-  }
- 
-});
+
 client.login(process.env.BOT_TOKEN);
