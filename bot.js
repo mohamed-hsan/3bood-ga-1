c99cc283b97c25a1dac6554c824559b27004dcdf
mohamed-hsan <43474223+mohamed-hsan@users.noreply.github.com>
2018-09-24 10:52:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -197,5 +197,20 @@ const secre = [
   console.log('[id] Send By: ' + message.author.username)
     }
 });
+client.on('message', message => {
+if(message.content.startsWith("=slots")) {
+  let slot1 = ['🍏', '🍇', '🍒', '🍍', '🍅', '🍆', '🍑', '🍓'];
+  let slots1 = `${slot1[Math.floor(Math.random() * slot1.length)]}`;
+  let slots2 = `${slot1[Math.floor(Math.random() * slot1.length)]}`;
+  let slots3 = `${slot1[Math.floor(Math.random() * slot1.length)]}`;
+  let we;
+  if(slots1 === slots2 && slots2 === slots3) {
+    we = "Win!"
+  } else {
+    we = "Lose!"
+  }
+  message.channel.send(`${slots1} | ${slots2} | ${slots3} - ${we}`)
+}
+});
 
 client.login(process.env.BOT_TOKEN);
